ca5cda0b803b2036dfe248e1e3e580c9ffdb96e3
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2022-12-28 20:47:12 +0000

    updated service and controller for GET requests with id

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Post, Body, Header } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ProductDto } from '../dtos/ProductDto.dto';
 // import { ProductInterface } from 'src/types/IProduct';
 import { ProductService } from './products.service';
 
 @Controller('/products')
 export class ProductsContrller {
-  // handling incoming post request
+  // handling incoming post requests
   constructor(private readonly productService: ProductService) {}
+  
+  // create product by post requests
   @Post("create")
   addNewProduct(@Body() product: ProductDto) {
     console.log("-----", product)
     return this.productService.addNewProduct(product)
   }
+
+  // get the list of products
+  @Get("lists")
+  getAllProducts(){
+    return this.productService.getAllProducts();
+  }
+
+  //get specific data by id
+  @Get("lists/:id")
+  getSpecificProduct(@Param("id") id:string){
+    return this.productService.getSpecificProduct(id)
+  }
+
+  // post to specific data - with id
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,9 +6,20 @@ import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class ProductService {
   products: ProductDto[] = [];
+  private uniqueId : number = 0;
   addNewProduct(product: ProductDto) {
-    product["id"] = uuidv4()
+    this.uniqueId++;
+    product['id'] = this.uniqueId.toString()
     this.products.push(product);
     return this.products;
+  }
+
+  getAllProducts() {
+    return this.products;
+  }
+
+  getSpecificProduct(productId: string){
+   let getProductById = this.products.filter(eachProduct => eachProduct.id === productId)
+   return getProductById
   }
 }
