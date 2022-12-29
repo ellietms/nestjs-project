ffbd3618f75c089ebd121080e329a2cf12f4294e
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2022-12-29 10:22:00 +0000

    deleted extra package in my file - uuid

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ProductDto } from '../dtos/ProductDto.dto';
 // import { ProductInterface } from 'src/types/IProduct';
-import { v4 as uuidv4 } from 'uuid';
+
 
 @Injectable()
 export class ProductService {
