3e0fefdffa9fad16e11e11a53b10416f5f168a0a
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2023-01-01 21:59:23 +0000

    created entity for tables in database

---
+++ src/typeorm/Product.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn({
+    name: 'product_id',
+  })
+  id: string;
+
+  @Column({
+    nullable: false,
+    default: ""
+  })
+  name: string;
+
+  @Column({
+    nullable: false,
+    default: ""
+  })
+  description: string;
+
+  @Column({
+    nullable: false
+  })
+  price: number;
+}
