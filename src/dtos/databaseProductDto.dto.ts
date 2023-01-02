cada5cb8fdb1636064a367a58d3afd229e6804fe
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2023-01-02 20:32:28 +0000

    added validation for database DTO

--- src/dtos/databaseProductDto.dto.ts
+++ src/dtos/databaseProductDto.dto.ts
@@ -1,12 +1,22 @@
-export class databaseProductDto {
-    
+import { IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
 
+export class databaseProductDto {
+    @IsNotEmpty()
+    @IsString()
     id: string;
     
+    @IsNotEmpty()
+    @MinLength(3)
+    @IsString()
     name: string;
     
+    @IsNotEmpty()
+    @MinLength(5)
+    @IsString()
     description: string;
     
+    @IsNotEmpty()
+    @IsNumber()
     price: number;
   }
    
